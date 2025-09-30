f79585030dac6ddf0b8579b1af69c77d26b7d1c7
Bryan Gordon <bryanbge92@gmail.com>
2025-09-29 22:18:28 -0500

    Create button for delete users

---
+++ src/components/DeleteUserButton.svelte
@@ -0,0 +1,47 @@
+<script lang="ts">
+  export let userId: string
+
+  function handleDelete() {
+    try {
+      fetch(`http://localhost:3000/users/${userId}`, {
+        method: 'DELETE'
+      })
+      alert("Usuario borrado correctamente")
+      window.location.href = '/'
+    } catch (e) {
+      console.log(e)
+      alert("No se ha podido borrar al usuario")
+    }
+  }
+</script>
+
+<div>
+  <button on:click={handleDelete}>Borrar</button>
+</div>
+
+<style>
+  div {
+    margin-block: 25px;
+    align-items: center;
+    text-align: center;
+  }
+
+  button:hover {
+    background-color: #dc143ca4;
+    color: white;
+  }
+
+  button {
+    height: 45px;
+    border: 0;
+    line-height: 25px;
+    padding: 10px 15px;
+    background: crimson;
+    color: whitesmoke;
+    border-radius: 4px;
+    text-decoration: none;
+    transition: all 0.3s ease;
+    cursor: pointer;
+  }
+</style>
+
